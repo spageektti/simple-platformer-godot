08d6df4cf44eb81705cb0a7fc50f7c04f510969c
spageektti <155078792+spageektti@users.noreply.github.com>
2024-08-23 00:04:54 +0200

    Continue creating lvl 4

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -9,3 +9,6 @@ func _on_level_2_pressed():
 
 func _on_level_3_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level3.tscn")
+
+func _on_level_4_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level4.tscn")
