cf2cc16b84128e2da91df36de8eea0f58645cdcc
spageektti <155078792+spageektti@users.noreply.github.com>
2024-08-23 20:28:05 +0200

    finish hour

--- game_manager.gd
+++ game_manager.gd
@@ -1,7 +1,8 @@
 extends Node
 
-var points = 0
-var lives = 3.0
+var points := 0
+var lives := 3.0
+var time := 0.0
 
 var full_heart = load("res://assets/objects/hearts/heart.png")
 var half_heart = load("res://assets/objects/hearts/half_heart.png")
@@ -9,6 +10,7 @@ var no_heart = load("res://assets/objects/hearts/no_heart.png")
 
 @export var hearts : Array[Node]
 @onready var points_label = %pointsLabel
+@onready var timer = %timer
 
 func decrease_health():
 	lives -= 0.5
@@ -27,3 +29,7 @@ func add_point():
 	points += 1
 	print(points)
 	points_label.text = "Points: " + str(points)
+	
+func _process(delta):
+	time += delta
+	timer.text = str(time)
